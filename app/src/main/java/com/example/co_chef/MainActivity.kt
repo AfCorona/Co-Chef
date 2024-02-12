fc6e125259c9a0677e8307828aa0e5b1606fd75c
Adolfo Corona <afcorona@csu.fullerton.edu>
2024-02-11 19:09:34 -0800

    First commit

--- app/src/main/java/com/example/co_chef/MainActivity.kt
+++ app/src/main/java/com/example/co_chef/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.co_chef
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val next = findViewById<ConstraintLayout>(R.id.title)
+
+        next.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
 }
